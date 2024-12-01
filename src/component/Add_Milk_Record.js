a6ed964513915da01c329686694900b67749e8f0
Bhushan Jadhav <bhu3203298s007@gmail.com>
2024-12-02 00:06:33 +0530

    Viarj

--- src/component/Add_Milk_Record.js
+++ src/component/Add_Milk_Record.js
@@ -1,39 +1,55 @@
 import React from "react";
 import "./Add_Milk_Record.css";
+import  milk_can from "../assets/milk-can.jpg"
 export default function Add_Milk_Record() {
   return (
     <>
       {/* <div className="container"> */}
-          <div className="header ">
-              
-        <button
-          class="btn btn-secondary dropdown-toggle d-flex   "
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Admin
-        </button>
-        <ul class="dropdown-menu">
-          <li>
-            <a class="dropdown-item" href="/milk">
-              Farmer
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="/">
-              User
-            </a>
-          </li>
-        </ul>
-      </div>
+      <div className="header d-flex justify-content-between align-items-center p-3">
+  {/* Anchor Tags on the Left */}
+  <div className="nav-links">
+    <a className="nav-link" href="/">Generate Bills</a>
+    <a className="nav-link" href="/">Loan/Feed</a>
+  </div>
+
+  {/* Admin Dropdown on the Right */}
+  <div className="dropdown">
+    <button
+      className="btn btn-secondary dropdown-toggle"
+      type="button"
+      data-bs-toggle="dropdown"
+      aria-expanded="false"
+    >
+      Admin
+    </button>
+    <ul className="dropdown-menu">
+      <li>
+        <a className="dropdown-item" href="/milk">Farmer</a>
+      </li>
+      <li>
+        <a className="dropdown-item" href="/">User</a>
+      </li>
+    </ul>
+  </div>
+</div>
+      <div className="container1">
       <div className="row">
         <div className="col-xl-4  milk-collection">
           <form action="">
+            
+          <label htmlFor="">Time</label>
+          <select id="cattle_type" name="cattle_type" required>
+        <option value="cow">M</option>
+        <option value="buffalo">E</option>
+    </select>
+          <label htmlFor="">Cattle</label>
+          <select id="cattle_type" name="cattle_type" required>
+        <option value="cow">Cow</option>
+        <option value="buffalo">Buffalo</option>
+    </select> <br />
+       
             <label htmlFor="">Id No:</label>
-            <input type="text" />
-            <label htmlFor="">Catt</label>
-            <input type="text" /> <br />
+            <input type="text" /><br />
             <label htmlFor="">Name:</label>
             <input type="" className="name" /> <br />
             <label htmlFor="">Litre:</label>
@@ -55,9 +71,10 @@ export default function Add_Milk_Record() {
           <table>
             <thead>
               <tr>
-                <th>Number</th>
-                <th>Name</th>
+                <th>Id No</th>
+                <th>Time</th>
                 <th>Animal</th>
+                <th>Name</th>
                 <th>Liters</th>
                 <th>Fat</th>
                 <th>SNF</th>
@@ -68,8 +85,9 @@ export default function Add_Milk_Record() {
             <tbody>
               <tr>
                 <td>1</td>
-                <td>Banerjee</td>
+                <td>M</td>
                 <td>Buffalo</td>
+                <td>Banerjee</td>
                 <td>20.00</td>
                 <td>4.00</td>
                 <td>6.70</td>
@@ -83,7 +101,9 @@ export default function Add_Milk_Record() {
 
       {/* <div class="table-container"> */}
       <div class="row">
-        <div class="col-xl-4 milk-collection"></div>
+        <div class="col-xl-4 milk-collection">
+          <img src={milk_can} alt=""  className="img-fluid"/>
+        </div>
         <div class="col-xl-8">
           <table>
             <thead>
@@ -131,6 +151,7 @@ export default function Add_Milk_Record() {
             <span>F10 Report Shift</span>
             <span>F11 Local Sale</span>
         </div> */}
+      </div>
       </div>
       {/* </div> */}
     </>
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,81 +1,62 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./Header.css"
 export default function Header() {
 
   
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div
-            class="collapse navbar-collapse justify-content-end "
-            id="navbarNav"
-          >
-            <ul class="nav  ">
-              <li class="nav-item">
-                <Link
-                  class="nav-link active"
-                  aria-current="page"
-                  to="/password"
-                >
-                  Home
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/milk">
-                  About
-                </Link>
-              </li>
-
-              <li class="nav-item">
-                <Link class="nav-link" to="login">
-                  Login
-                </Link>
-              </li>
-            </ul>
-
+    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+      <div class="container-fluid">
+        <Link class="navbar-brand" to="#">Navbar</Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          {/* <!-- Navbar Links --> */}
+          <ul class="navbar-nav me-3">
+            <li class="nav-item">
+              <Link class="nav-link active" aria-current="page" to="/password">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/farmer">About</Link>
+            </li>
+          </ul>
+  
+          {/* <!-- Login Dropdown Button --> */}
+          <div class="dropdown">
             <button
-              class="btn btn-secondary dropdown-toggle d-flex   "
+              class="btn btn-secondary dropdown-toggle"
               type="button"
+              id="loginDropdown"
               data-bs-toggle="dropdown"
               aria-expanded="false"
             >
               Login
             </button>
-            <ul class="dropdown-menu">
+            <ul class="dropdown-menu" aria-labelledby="loginDropdown">
               <li>
-                <a class="dropdown-item" href="/login">
-                  Admin
-                </a>
+                <Link class="dropdown-item" to="/login">Admin</Link>
               </li>
               <li>
-                <a class="dropdown-item" href="/">
-                  User
-                </a>
+                <Link class="dropdown-item" to="/">User</Link>
               </li>
             </ul>
-            {/* <button class="login-button" onclick="toggleDropdown()">Login</button>
-        <div class="dropdown-menu" id="loginOptions" style="display: none;">
-            <a href="#admin-login">Admin Login</a>
-            <a href="#user-login">User Login</a>
-        </div> */}
           </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
+  </div>
+  
   );
 }
